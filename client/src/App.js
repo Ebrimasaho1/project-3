c7b313d1a760dca6cc1a9fba19874cb2876b3157
Ebrimasaho1 <saho.ebrima@ymail.com>
2019-07-17 20:52:52 -0700

    Empty project with basic routes

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import Search from "./pages/Search";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <Router>
+      <div>
+       
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/dashboard" component={Dashboard}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
