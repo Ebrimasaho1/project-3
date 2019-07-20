67ca6eade925636308a6f35cacfdbde138ea837c
Laura B <laurabermudezg@gmail.com>
2019-07-20 14:33:52 -0700

    Fix routes

--- Scripts/SeedDB.js
+++ Scripts/SeedDB.js
@@ -6,12 +6,10 @@ mongoose.connect(
   "mongodb://localhost/revolve"
 );
 
-
-
 db.LessonPlan.create([{ title: 'Test Lesson Plan' }])
   .then(([lessonPlans]) => {
     Promise.all([
-      db.User.create([{ email: "guest@gmail.com", lessonPlans: lessonPlans[0] }]),
+      db.User.create([{ email: "guest@gmail.com", fullName: "John Doe",lessonPlans: lessonPlans[0] }]),
       db.Project.create([{ name: 'Webelos Den', lessonPlans: lessonPlans[0] }])
     ]).then(([projects]) => {
       return db.Organization.create([{ name: 'Cub Scouts Pack', projects: projects[0] }]);
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userRoutes = require("./login");
 const lessonPlansRoutes = require("./lessonPlans");
 const organizationsRoutes = require("./organizations");
+const projectsRoutes = require("./projects");
 
 router.use("/login", userRoutes);
 router.use("/lessonPlans", lessonPlansRoutes);
