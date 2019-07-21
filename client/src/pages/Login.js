dfd717ed5a1939a92b05151f64a8b343eeebde53
Laura B <laurabermudezg@gmail.com>
2019-07-20 22:35:36 -0700

    saving login to local storage

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -14,6 +14,7 @@ class Login extends Component {
       var user = {fullName: response.w3.ig, email: response.w3.U3};
       sessionStorage.setItem("currentUser", JSON.stringify(user));
       api.saveUser(user);
+      //redirect to dashboard
     }
     return(
       <GoogleLogin
