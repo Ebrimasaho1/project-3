1599c4a66efcde6335690a1945819baf9b21f4ea
Laura B <laurabermudezg@gmail.com>
2019-07-22 19:01:04 -0700

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route exact path="/" component={Login} />
           <Route exact path="/search" component={Search} />
           <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/form" component={Form} />
+          <Route exact path="/lessonPlan" component={Form} />
         </Switch>
       </div>
     </Router>
