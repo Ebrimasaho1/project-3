6684cc9ea68840d41fca2939d0ff9e10905244d1
Mark Silva <mark.allen.silva@gmail.com>
2019-07-29 20:54:44 -0700

    basic form validation added

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -28,9 +28,16 @@ class Form extends Component {
       materials: "",
       description: "",
       organization: "",
+      titleError: "",
+      objectiveError: "",
+      overviewError: "",
+      preparationError: "",
+      agendaError: "",
+      materialsError: "",
+      descriptionError: "",
 
       lessonId: props.lessonId,
-      errors: [],
+      errors: {},
 
       isModalOpen: false,
       addOperation: "",
@@ -88,7 +95,7 @@ class Form extends Component {
   componentDidMount() {
     this.loadOrganizations();
     this.loadLessonPlan();
-    if(this.state.selectedOrganization){
+    if (this.state.selectedOrganization) {
       this.populateProjectsForSelectedOrg(this.state.selectedOrganization);
     }
   }
@@ -107,7 +114,7 @@ class Form extends Component {
         }
       }
       this.setState({
-        projsOptions : projsOptions
+        projsOptions: projsOptions
       });
     });
   }
@@ -131,9 +138,9 @@ class Form extends Component {
     this.state.organizationOpts.push(newOrg);
 
     this.setState({
-      organizationOpts : this.state.organizationOpts,
-      selectedOrganization: id, 
-      selectedOrg : newOrg
+      organizationOpts: this.state.organizationOpts,
+      selectedOrganization: id,
+      selectedOrg: newOrg
     });
     this.setState(
       this.state
@@ -142,13 +149,13 @@ class Form extends Component {
 
   setSelectedProject = (name, id) => {
     //function to update the project options in select -- not functional
-    var newProj = {value: id, label: name};
+    var newProj = { value: id, label: name };
     this.state.projsOptions.push(newProj);
 
     this.setState({
-      projsOptions : this.state.projsOptions,
+      projsOptions: this.state.projsOptions,
       selectedProject: id,
-      selectedProj : newProj
+      selectedProj: newProj
     });
     this.setState(
       this.state
@@ -158,13 +165,52 @@ class Form extends Component {
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
-      [name]: value
+      [name]: value,
+      titleError: "",
+      objectiveError: "",
+      overviewError: "",
+      preparationError: "",
+      agendaError: "",
+      materialsError: "",
+      descriptionError: ""
     });
   };
 
 
   handleFormSubmit = event => {
     event.preventDefault();
+
+    let errors = {};
+    errors.errorFree = true;
+
+    if (!this.state.title) {
+      errors.title = "Enter lesson title";
+      errors.errorFree = false
+    }
+    if (!this.state.objective) {
+      errors.objective = "Enter lesson objective";
+      errors.errorFree = false
+    }
+    if (!this.state.overview) {
+      errors.overview = "Enter lesson overview";
+      errors.errorFree = false
+    }
+    if (!this.state.preparation) {
+      errors.preparation = "Enter lesson preparation";
+      errors.errorFree = false
+    }
+    if (!this.state.agenda) {
+      errors.agenda = "Enter lesson agenda";
+      errors.errorFree = false
+    }
+    if (!this.state.materials) {
+      errors.materials = "Enter lesson materials";
+      errors.errorFree = false
+    }
+    if (!this.state.description) {
+      errors.description = "Enter lesson description";
+      errors.errorFree = false
+    }
 
     console.log(`
     Lesson Title: ${this.state.title}\n
@@ -177,36 +223,48 @@ class Form extends Component {
     Project: ${this.state.selectedProject}
     `);
 
-    var userId = sessionStorage.getItem("currentUserId");
+    if (errors.errorFree) {
+      var userId = sessionStorage.getItem("currentUserId");
 
-    var lessonPlan = {
-      title: this.state.title,
-      objective: this.state.objective,
-      overview: this.state.overview,
-      preparation: this.state.preparation,
-      agenda: this.state.agenda,
-      materials: this.state.materials,
-      description: this.state.description,
-      project: this.state.selectedProject,
-      user: userId
-    };
+      var lessonPlan = {
+        title: this.state.title,
+        objective: this.state.objective,
+        overview: this.state.overview,
+        preparation: this.state.preparation,
+        agenda: this.state.agenda,
+        materials: this.state.materials,
+        description: this.state.description,
+        project: this.state.selectedProject,
+        user: userId
+      };
 
-    // function validate(lessonTitle) {
-    //   const errors = [];
+      // function validate(lessonTitle) {
+      //   const errors = [];
 
-    //   if (lessonTitle.length === 0) {
-    //     errors.push("Lesson Title can't be empty");
-    //   }
-    //   return errors;
-    // }
+      //   if (lessonTitle.length === 0) {
+      //     errors.push("Lesson Title can't be empty");
+      //   }
+      //   return errors;
+      // }
 
-    //call api.saveLessonPlan(lessonPlan).then...
-    api.saveLessonPlan(lessonPlan).then((result) => {
-      console.log("Lesson Plan saved");
-    });
+      //call api.saveLessonPlan(lessonPlan).then...
+      api.saveLessonPlan(lessonPlan).then((result) => {
+        console.log("Lesson Plan saved");
+      });
+    } else {
+      this.setState({
+        titleError: errors.title,
+        objectiveError: errors.objective,
+        overviewError: errors.overview,
+        preparationError: errors.preparation,
+        agendaError: errors.agenda,
+        materialsError: errors.materials,
+        descriptionError: errors.description
+      })
+    }
   };
 
- 
+
 
   openModal = (operation) => {
     //console.log("operation in open modal:" + operation);
@@ -229,6 +287,7 @@ class Form extends Component {
           <input type="text" className="form-control" id="title" placeholder=""
             name="title" value={this.state.title} onChange={this.handleInputChange}></input>
         </h1>
+        <span className="error">{this.state.titleError}</span>
 
         <div className="d-flex justify-content-around">
           <label>Organization</label>
@@ -239,8 +298,8 @@ class Form extends Component {
           <button type="button" className="btn btn-secondary" id="addNew" onClick={() => { this.openModal("Organization") }}>
             Add New
            </button>
-          <AddModal selectedOrganization={this.state.selectedOrganization} addOperation={this.state.addOperation} isModalOpen={this.state.isModalOpen} closeModal={this.closeModal} 
-          setSelectedOrganization = {this.updateOrgOptions} setSelectedProject = {this.setSelectedProject}/>
+          <AddModal selectedOrganization={this.state.selectedOrganization} addOperation={this.state.addOperation} isModalOpen={this.state.isModalOpen} closeModal={this.closeModal}
+            setSelectedOrganization={this.updateOrgOptions} setSelectedProject={this.setSelectedProject} />
 
           <label>Projects</label>
           <Select className="proj-select" name="proj" form="projects" type="list"
@@ -255,10 +314,12 @@ class Form extends Component {
         <label>Objective</label>
         <textarea type="text" className="form-control" id="objective" placeholder=""
           name="objective" value={this.state.objective} onChange={this.handleInputChange}></textarea>
+        <span className="error">{this.state.objectiveError}</span>
 
         <label>Overview</label>
         <textarea type="text" className="form-control" id="overview" placeholder=""
           name="overview" value={this.state.overview} onChange={this.handleInputChange}></textarea>
+        <span className="error">{this.state.overviewError}</span>
 
         {/* <label>Preparation</label>
         <textarea type="text" className="form-control" id="preparation" placeholder=""
@@ -267,14 +328,17 @@ class Form extends Component {
         <label>Agenda</label>
         <textarea type="text" className="form-control" id="agenda" placeholder=""
           name="agenda" value={this.state.agenda} onChange={this.handleInputChange}></textarea>
+          <span className="error">{this.state.agendaError}</span>
 
         <label>Materials</label>
         <textarea type="text" className="form-control" id="materials" placeholder=""
           name="materials" value={this.state.materials} onChange={this.handleInputChange}></textarea>
+          <span className="error">{this.state.materialsError}</span>
 
         <label>Description</label>
         <textarea type="text" className="form-control" id="description" placeholder=""
           name="description" value={this.state.description} onChange={this.handleInputChange}></textarea>
+          <span className="error">{this.state.descriptionError}</span>
 
         {/* <div className="d-flex justify-content-around">
           <label>Links</label>
